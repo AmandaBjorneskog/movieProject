79b8f77ec79d28b536e1167063a772159b8fd04d
Amanda Björneskog <mandolina_95@hotmail.com>
2016-07-28 14:02:42 +0200

    Finished code
    movies.txt is the list of movies 
    randomizeMovies.py is the script that randomizes the movie and removes it from the list

---
+++ randomizeMovies.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import statistics
+import re
+import random
+import linecache
+import time
+
+filename = "movies.txt"
+arr = []
+nrOfLines = 0
+randomNr = 0
+
+with open(filename) as myFile:
+    for num, line in enumerate(myFile,1):
+        nrOfLines = nrOfLines +1
+
+with open(filename) as f:
+    arr = f.readlines()
+    
+randomNr = random.randint(1,(nrOfLines-1))
+
+print(arr[randomNr])
+               
+    
+for i in range(randomNr, nrOfLines-1):
+    arr[i] =  arr[i+1]
+
+with open(filename, 'w') as file:
+    file.writelines(arr)
